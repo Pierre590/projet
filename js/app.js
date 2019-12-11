cb0318387cc0decf2f7fe0c07ec46c6c92f0016e
pierre <nanashi4781@gmail.com>
2019-12-11 08:51:06 +0100

    prepare

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$(".button1").click(function(){
+$(".bg-info1").toggle();
+});
+
+$(".button2").click(function(){
+$(".bg-info2").toggle();
+});
+    
+$(".button3").click(function(){
+$(".bg-info3").toggle();
+});
